e176f35fbc9ea3f91889ac9a80aaca0bfd47a01a
xiaoxia <920634755@qq.com>
2018-07-20 12:57:18 +0800

    首页完成

--- src/route/Msite/Msite.vue
+++ src/route/Msite/Msite.vue
@@ -67,7 +67,7 @@
 
        </div>
       <div class="shop_pinpai">
-         <div class="gray"></div>
+
           <div class="hd">
             <span>品牌制造商直供<i class="iconfont icon-shouqijiantouxiao-copy"></i></span>
           </div>
@@ -281,6 +281,125 @@
           </div>
         </div>
       </div>
+      <div class="xianshishop">
+        <div class="xianshishop_item left">
+          <div class="title">
+            <span>严选限时购</span>
+          </div>
+          <div class=" Countdown_time">
+            <span class="hour">01</span>
+            <span>:</span>
+            <span class="min">01</span>
+            <span>:</span>
+            <span class="second">01</span>
+          </div>
+          <div class="predict_time">
+            <span class="up_time">下一场</span>
+            <span class="predict_time_center">10:00</span>
+            <span class="begin_time">开始</span>
+          </div>
+        </div>
+        <div class=" xianshishop_item right">
+          <div class="right_img">
+            <img src="./images/shafa.png" alt="">
+          </div>
+          <div class="circle">
+            <div class="new">￥9999</div>
+            <div class="old">￥11999</div>
+          </div>
+        </div>
+      </div>
+      <div class="fuli">
+        <div>
+          <img src="./images/fuli.jpg" alt="">
+        </div>
+      </div>
+      <div class="zhuanti">
+        <div class="zhuanti_title">
+          <div class="title">
+            <span>专题精选</span>
+            <i class="iconfont icon-shouqijiantouxiao-copy"></i>
+          </div>
+        </div>
+        <div class="bottom_wrapper">
+          <ul class="bottom_slide">
+            <li>
+              <img src="./images/shuiguo.jpg" alt="">
+              <h4>青汁兑蜂蜜, 轻盈又甜蜜</h4>
+
+              <div class="small">每天来杯青汁蜂蜜水，给生活添一点甜蜜</div>
+              <span>129元起</span>
+            </li>
+            <li>
+              <img src="./images/xinglixiang.jpg" alt="">
+              <h4>青汁兑蜂蜜, 轻盈又甜蜜</h4>
+
+              <div class="small">每天来杯青汁蜂蜜水，给生活添一点甜蜜</div>
+              <span>129元起</span>
+            </li>
+
+          </ul>
+        </div>
+      </div>
+      <div class="jujiahaowu">
+        <div class="jujiahaowu_title">
+          <div class="jujiahaowu_title_inner">
+            <h3>居家好物</h3>
+          </div>
+        </div>
+        <div class="jujiahaowu_content">
+          <ul class="jujiahaowu_content_inner">
+            <li>
+              <img src="./images/mianbei.jpg" alt="">
+              <div class="ul-item li_inner">透气吸汗麻+亲肤柔软棉</div>
+              <div class="ul-item li_info">自然棉麻四件套</div>
+              <div class="ul-item li_price">￥599</div>
+            </li>
+            <li>
+              <img src="./images/mianbei.jpg" alt="">
+              <div class="ul-item li_inner">透气吸汗麻+亲肤柔软棉</div>
+              <div class="ul-item li_info">自然棉麻四件套</div>
+              <div class="ul-item li_price">￥599</div>
+            </li>
+            <li>
+              <img src="./images/mianbei.jpg" alt="">
+              <div class="ul-item li_inner">透气吸汗麻+亲肤柔软棉</div>
+              <div class="ul-item li_info">自然棉麻四件套</div>
+              <div class="ul-item li_price">￥599</div>
+            </li>
+            <li>
+              <img src="./images/mianbei.jpg" alt="">
+              <div class="ul-item li_inner">透气吸汗麻+亲肤柔软棉</div>
+              <div class="ul-item li_info">自然棉麻四件套</div>
+              <div class="ul-item li_price">￥599</div>
+            </li>
+            <li>
+              <img src="./images/mianbei.jpg" alt="">
+              <div class="ul-item li_inner">透气吸汗麻+亲肤柔软棉</div>
+              <div class="ul-item li_info">自然棉麻四件套</div>
+              <div class="ul-item li_price">￥599</div>
+            </li>
+            <li>
+              <img src="./images/mianbei.jpg" alt="">
+              <div class="ul-item li_inner">透气吸汗麻+亲肤柔软棉</div>
+              <div class="ul-item li_info">自然棉麻四件套</div>
+              <div class="ul-item li_price">￥599</div>
+            </li>
+            <li>
+              <img src="./images/mianbei.jpg" alt="">
+              <div class="ul-item li_inner">透气吸汗麻+亲肤柔软棉</div>
+              <div class="ul-item li_info">自然棉麻四件套</div>
+              <div class="ul-item li_price">￥599</div>
+            </li>
+            <li>
+              <img src="./images/mianbei.jpg" alt="">
+              <div class="ul-item li_inner">透气吸汗麻+亲肤柔软棉</div>
+              <div class="ul-item li_info">自然棉麻四件套</div>
+              <div class="ul-item li_price">￥599</div>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
 
   </div>
@@ -296,14 +415,22 @@
     mounted(){
       this.$nextTick(()=>{
         new BScroll('.nav_wrapper',{
-          scrollX:true
+          scrollX:true,
+          eventPassthrough:'vertical'
         })
         new BScroll('.inner',{
-          scrollX:true
+          scrollX:true,
+          eventPassthrough:'vertical'
         })
         new BScroll('.hot_inner',{
-          scrollX:true
+          scrollX:true,
+          eventPassthrough:'vertical'
         })
+        new BScroll('.bottom_wrapper',{
+          scrollX:true,
+          eventPassthrough:'vertical'
+        })
+
       })
 
       new Swiper('.swiper-container',{
@@ -332,7 +459,8 @@
 <style lang='less' rel="stylesheet/less" scoped>
 @import "../../assets/less/mixins";
   .msite{
-  //  background: #eee;
+    overflow: hidden;
+
     .header_wrapper{
       position: relative;
       z-index: 100;
@@ -343,7 +471,8 @@
         left: 0;
         background: white;
         .header{
-          width: 750/@rem;
+       //   width: 750/@rem;
+          width: 100%;
           height:88/@rem;
           // background: pink;
           box-sizing: border-box;
@@ -412,11 +541,13 @@
       }
     }
     .msite_content{
+      background: #eee;
       margin-top:148/@rem ;
       margin-bottom: 200px;
       .swiper-container{
         width: 750/@rem;
         height: 400/@rem;
+        background: #fff;
           .swiper-wrapper{
             .swiper-slide{
               &>img{
@@ -430,6 +561,7 @@
         display: flex;
         height: 72/@rem;
         align-items: center;
+        background: #fff;
         &>div{
           flex: 1;
           text-align: center;
@@ -446,12 +578,10 @@
       }
       .shop_pinpai{
         width: 750/@rem;
-        height: 586/@rem;
-        .gray{
-          width: 750/@rem;
-          height: 20/@rem;
-          background: #eee;
-        }
+        height: 610/@rem;
+        background: #fff;
+        margin-top: 10px;
+
         .hd{
           width: 750/@rem;
           height: 110/@rem;
@@ -468,6 +598,8 @@
           .clearFix();
           width: 750/@rem;
           height: 476/@rem;
+          background: #fff;
+
           &>.item{
             width: 355/@rem;
             height: 238/@rem;
@@ -496,7 +628,7 @@
          width: 750/@rem;
          height: 772/@rem;
          //  background-color: green;
-         margin-top: 30px;
+         margin-top: 10px;
          .new_title{
            width: 750/@rem;
            height: 260/@rem;
@@ -536,9 +668,11 @@
            .inner{
              width: 750/@rem;
              height: 480/@rem;
+             overflow: hidden;
+             background-color: white;
              .new_wrapper{
                .clearFix();
-               width: 2000/@rem;
+               width: 380%;
                height: 414/@rem;
                .item_slide{
                  float: left;
@@ -583,7 +717,7 @@
          width: 750/@rem;
          height: 772/@rem;
          background-color: #fffffa;
-         margin-top: -20px;
+         margin-top: 5px;
          .new_title{
            width: 750/@rem;
            height: 260/@rem;
@@ -621,11 +755,12 @@
          //滑动
          .new_slide{
            .hot_inner{
+             overflow: hidden;
              width: 750/@rem;
              height: 480/@rem;
              .new_wrapper{
                .clearFix();
-               width: 2000/@rem;
+               width: 380%;
                height: 414/@rem;
                .item_slide{
                  float: left;
@@ -666,6 +801,207 @@
            }
          }
        }
+      .xianshishop{
+        width: 750/@rem;
+        height: 380/@rem;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background: #fff;
+        margin-top: 10px;
+        .left{
+          width: 334/@rem;
+          height: 320/@rem;
+          color:#333;
+          .title{
+            margin-top: 40px;
+            color: #333;
+            font-size: 20px;
+          }
+          .Countdown_time{
+            margin: 20px 0;
+            color: white;
+            font-size: 20px;
+            .hour{
+              width: 62/@rem;
+              height: 56/@rem;
+              background: #333;
+            }
+            .min{width: 62/@rem;
+              height: 56/@rem;
+              background: #333;}
+            .second{width: 62/@rem;
+              height: 56/@rem;
+              background: #333;
+            }
+          }
+          .predict_time{
+            font-size: 16px;
+            color: #333;
+          }
+        }
+        .right{
+          width: 320/@rem;
+          height: 320/@rem;
+          position: relative;
+          .right_img{
+            &>img{
+              width: 100%;
+              height: 100%;
+              vertical-align: middle;
+            }
+          }
+          .circle{
+            width: 96/@rem;
+            height: 96/@rem;
+            background: rgba(244,143,24,.95);
+            color: #fff;
+            font-size: 10px;
+            border-radius: 50px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            position: absolute;
+            bottom: 0;
+            right: 0;
+            .new{
+              position: absolute;
+              left: 0;
+              top: 0;
+            }
+            .old{
+              position: absolute;
+              left: 0;
+              top: 20px;
+              text-decoration: line-through;
+            }
+          }
+        }
+
+      }
+      .fuli{
+        width: 750/@rem;
+        height: 300/@rem;
+        background: pink;
+        margin-top: 10px;
+        &>div{
+          img{
+            width: 100%;
+            vertical-align: middle;
+          }
+        }
+      }
+      .zhuanti{
+        width: 750/@rem;
+        height: 563/@rem;
+        margin-top: 10px;
+        background: #fff;
+        .zhuanti_title{
+          overflow: hidden;
+          width: 750/@rem;
+          height: 110/@rem;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          .title{
+            font-size:16px ;
+            color: #333;
+          }
+        }
+        .bottom_wrapper{
+          .bottom_slide{
+            width: 200%;
+            height: 417/@rem;
+            font-size: 16px;
+
+            &>li{
+              width: 575/@rem;
+              height: 322/@rem;
+              margin-left: 15px;
+              float: left;
+              img{
+                width: 100%;
+                height: 100%;
+                vertical-align: middle;
+                border-radius: 10px;
+              }
+              h4{
+                margin: 3px 0;
+                color: #333;
+                width: 410/@rem;
+                height: 41/@rem;
+              }
+              span{
+                width: 102/@rem;
+                height: 41/@rem;
+                font-size:16px ;
+                color: red;
+              }
+              .small{
+                font-size: 14px;
+                color: #aaa;
+                margin-bottom:3px ;
+              }
+            }
+          }
+        }
+      }
+      .jujiahaowu{
+        overflow: hidden;
+        width: 750/@rem;
+        height: 2318/@rem;
+        background: white;
+        margin-top: 10px;
+        margin-bottom: -150px;
+        .jujiahaowu_title{
+          width: 750/@rem;
+          height: 120/@rem;
+          font-size:16px ;
+          display: flex;
+          color: #333;
+          justify-content: center;
+          align-items: center;
+
+        }
+        .jujiahaowu_content{
+          .jujiahaowu_content_inner{
+            &>li{
+              width: 375/@rem;
+              height: 537/@rem;
+             // margin: 10px;
+              float: left;
+              .ul-item{
+                font-size: 14px;
+                margin-left: 5px;
+
+              }
+              img{
+                width: 345/@rem;
+                height: 345/@rem;
+                vertical-align: middle;
+                margin-left: 8px;
+              }
+              .li_inner{
+                color: #9F8A60;
+                font-size: 10px;
+                width:345/@rem;
+                margin-left: 8px;
+                background-color:rgba(159,138,96,0.5);
+              }
+              .li_info{
+                color: #333;
+                font-size: 14px;
+                margin: 5px 5px;
+              }
+              .li_price{
+                color: red;
+                font-size: 14px;
+              }
+
+            }
+          }
+        }
+      }
     }
   }
 
