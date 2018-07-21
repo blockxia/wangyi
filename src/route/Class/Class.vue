7b1c91b05989e3663b5dbc6e1eac8cf672419649
xiaoxia <920634755@qq.com>
2018-07-21 23:22:18 +0800

    msite交互完成

--- src/route/Class/Class.vue
+++ src/route/Class/Class.vue
@@ -2,9 +2,6 @@
 
 <div class="cate">
   <div  class="header">
-    <div class="logo">
-      <img src="./images/logo.png" alt="" @click="$router.replace('/msite')">
-    </div>
     <div class="search">
       <i class="iconfont icon-search"></i>
       <span class="placeholder">搜索商品, 共10718款好物</span>
@@ -20,37 +17,30 @@
 
 <style lang='less' rel="stylesheet/less" scoped>
   @import "../../assets/less/mixins";
-  .header{
-    //   width: 750/@rem;
-    width: 100%;
-    height:88/@rem;
-    // background: pink;
-    box-sizing: border-box;
-    padding: 8px 15px;
-    display: flex;
-    .logo{
-      width: 138/@rem;
-      height:40/@rem;
-      img{
+  .cate{
+    background: #fff;
+    .header{
+      //   width: 750/@rem;
+      width: 100%;
+      height:88/@rem;
+      // background: pink;
+      box-sizing: border-box;
+      padding: 10px;
+      margin-top: -5px;
+      .search{
         width: 100%;
-        vertical-align: middle;
-      }
-    }
-    .search{
-      width: 532/@rem;
-      height:56/@rem;
-      background: #EDEDED;
-      text-align: center;
-      margin-left: 10px;
-      border-radius: 5px;
-      .iconfont{
+        height:56/@rem;
+        background: #EDEDED;
+        text-align: center;
+        border-radius: 5px;
+        .placeholder{
+          font-size: 12px;
+         vertical-align: middle;
+          color:#666;
 
-      }
-      .placeholder{
-        font-size: 14px;
-        vertical-align: middle;
-        color:#666
+        }
       }
     }
   }
+
 </style>
