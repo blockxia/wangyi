19944b844d0b6d1028c141350d7bbd44fac1343a
xiaoxia <920634755@qq.com>
2018-07-21 23:04:19 +0800

    msite交互完成

--- src/route/Class/Class.vue
+++ src/route/Class/Class.vue
@@ -1,6 +1,16 @@
 <template>
 
-
+<div class="cate">
+  <div  class="header">
+    <div class="logo">
+      <img src="./images/logo.png" alt="" @click="$router.replace('/msite')">
+    </div>
+    <div class="search">
+      <i class="iconfont icon-search"></i>
+      <span class="placeholder">搜索商品, 共10718款好物</span>
+    </div>
+  </div>
+</div>
 
 </template>
 
@@ -8,6 +18,39 @@
   export default {}
 </script>
 
-<style >
+<style lang='less' rel="stylesheet/less" scoped>
+  @import "../../assets/less/mixins";
+  .header{
+    //   width: 750/@rem;
+    width: 100%;
+    height:88/@rem;
+    // background: pink;
+    box-sizing: border-box;
+    padding: 8px 15px;
+    display: flex;
+    .logo{
+      width: 138/@rem;
+      height:40/@rem;
+      img{
+        width: 100%;
+        vertical-align: middle;
+      }
+    }
+    .search{
+      width: 532/@rem;
+      height:56/@rem;
+      background: #EDEDED;
+      text-align: center;
+      margin-left: 10px;
+      border-radius: 5px;
+      .iconfont{
 
+      }
+      .placeholder{
+        font-size: 14px;
+        vertical-align: middle;
+        color:#666
+      }
+    }
+  }
 </style>
